e85f2b85ac3362070ddbdb567ca590b707482cc2
jiseongg <jiseongg301@gmail.com>
2020-10-30 01:21:15 +0900

    [domain] implement point-wise state operation

--- domain/state.go
+++ domain/state.go
@@ -31,3 +31,63 @@ func (s *State) Find(x string) Interval {
 	}
 	return v
 }
+
+// point-wise order
+func StateOrder(s1, s2 State) bool {
+	for k, v1 := range s1 {
+		v2, ok := s2[k]
+		if !ok {
+			v2 = InterBot()
+		}
+		if !InterOrder(v1, v2) {
+			return false
+		}
+	}
+	return true
+}
+
+// point-wise operation
+func StateJoin(s1, s2 State) State {
+	s3 := EmptyState()
+	for k, v := range s2 {
+		s3[k] = v
+	}
+	for k, v1 := range s1 {
+		v2, ok := s3[k]
+		if !ok {
+			v2 = InterBot()
+		}
+		s3[k] = InterJoin(v1, v2)
+	}
+	return s3
+}
+
+func StateWiden(s1, s2 State) State {
+	s3 := EmptyState()
+	for k, v := range s2 {
+		s3[k] = v
+	}
+	for k, v1 := range s1 {
+		v2, ok := s3[k]
+		if !ok {
+			v2 = InterBot()
+		}
+		s3[k] = InterWiden(v1, v2)
+	}
+	return s3
+}
+
+func StateNarrow(s1, s2 State) State {
+	s3 := EmptyState()
+	for k, v := range s2 {
+		s3[k] = v
+	}
+	for k, v1 := range s1 {
+		v2, ok := s3[k]
+		if !ok {
+			v2 = InterBot()
+		}
+		s3[k] = InterNarrow(v1, v2)
+	}
+	return s3
+}
