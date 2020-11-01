0f44afef40b6e0c753f575ca6f50ce312f757fc8
jiseongg <jiseongg301@gmail.com>
2020-11-02 03:05:45 +0900

    [domain] implement semantics for Phi and ICmp

--- pkg/domain/semantics.go
+++ pkg/domain/semantics.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/llir/llvm/ir"
 	"github.com/llir/llvm/ir/constant"
+	"github.com/llir/llvm/ir/enum"
 	"github.com/llir/llvm/ir/value"
-	//	"github.com/llir/llvm/ir/enum"
 )
 
 func evalArgument(v value.Value, s *State) Interval {
@@ -42,6 +42,27 @@ func (s *State) transferInstMul(inst *ir.InstMul) {
 	s.Bind(loc, InterMult(vx, vy))
 }
 
+func (s *State) transferInstICmp(inst *ir.InstICmp) {
+	loc := inst.LocalIdent.Ident()
+	vx := evalArgument(inst.X, s)
+	vy := evalArgument(inst.Y, s)
+	itv := InterTop()
+	switch inst.Pred {
+	case enum.IPredSLT:
+		itv = InterSLT(vx, vy)
+	}
+	s.Bind(loc, itv)
+}
+
+func (s *State) transferInstPhi(inst *ir.InstPhi) {
+	loc := inst.LocalIdent.Ident()
+	itv := InterBot()
+	for _, inc := range inst.Incs {
+		itv = InterJoin(itv, evalArgument(inc.X, s))
+	}
+	s.Bind(loc, itv)
+}
+
 func (s *State) transferInst(inst ir.Instruction) {
 	switch inst := inst.(type) {
 	case *ir.InstAdd:
@@ -50,6 +71,10 @@ func (s *State) transferInst(inst ir.Instruction) {
 		s.transferInstSub(inst)
 	case *ir.InstMul:
 		s.transferInstMul(inst)
+	case *ir.InstPhi:
+		s.transferInstPhi(inst)
+	case *ir.InstICmp:
+		s.transferInstICmp(inst)
 	default:
 		fmt.Printf("Unsupported instructions: %T\n", inst)
 	}
